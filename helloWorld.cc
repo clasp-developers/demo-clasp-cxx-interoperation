81879273c2699fd33bd494b46a24ccff62d0e765
Chris Schafmeister <chris.schaf@verizon.net>
2014-10-02 12:24:31 -0400

    added a function that takes arguments and returns a value

--- helloWorld.cc
+++ helloWorld.cc
@@ -1,15 +1,10 @@
 //
 // Set up a clasp.h include file with all the good stuff
 //
-
-
 #include <stdio.h>
 #include "clasp.h"
 
-
-
 #define EXPORT __attribute__((visibility("default")))
-
 
 void helloWorld()
 {
@@ -17,6 +12,10 @@ void helloWorld()
 }
 
 
+double addThreeNumbers(double x, double y, double z)
+{
+    return x+y+z;
+}
 
 extern "C" {
     EXPORT
@@ -24,10 +23,9 @@ extern "C" {
     {
         printf("!\n!\n!\n!   Starting CLASP_MAIN\n!\n!\n");
         printf("Expose C++ code to Clasp here\n");
-
         clbind::package("HELLO") [
             clbind::def("hello-world",&helloWorld)
+            , clbind::def("addThreeNumbers",&addThreeNumbers)
             ];
     }
-
 }
