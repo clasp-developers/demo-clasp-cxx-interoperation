5999210601cb6d75fa7d0a939aad9950d18b0d28
Chris Schafmeister <chris.schaf@verizon.net>
2014-10-02 02:21:20 -0400

    added code to expose function - broken at the moment

--- helloWorld.cc
+++ helloWorld.cc
@@ -2,9 +2,9 @@
 // Set up a clasp.h include file with all the good stuff
 //
 
-//#include "clasp.h"
 
 #include <stdio.h>
+#include "clasp.h"
 
 
 
@@ -24,6 +24,10 @@ extern "C" {
     {
         printf("!\n!\n!\n!   Starting CLASP_MAIN\n!\n!\n");
         printf("Expose C++ code to Clasp here\n");
+
+        clbind::package("hello") [
+            clbind::def("hello-world",&helloWorld)
+            ];
     }
 
 }
