2877cf99875ed0cc2cbbb61d09155fd44d795bd6
drmeister <chris.schaf@verizon.net>
2014-12-13 08:23:29 -0500

    added an example of defining arguments for method and passing string

--- double-vector/double-vector.lsp
+++ double-vector/double-vector.lsp
@@ -2,4 +2,7 @@
 (setq a (dv:make-double-vector-with-values '(1 2 3)))
 (setq b (dv:make-double-vector-with-values '(4 5 6)))
 (format t "dot product of (1 2 3).(4 5 6) --> ~a~%" (dv:dot a b))
+(dv:fill a #(1.2 3.4 5.6))
+(dv:dump a "gratuitous string!")
+(dv:dump a )   ;; note: I left off the optional argument
 (quit)
--- double-vector/doubleVector.cc
+++ double-vector/doubleVector.cc
@@ -3,10 +3,13 @@
 //
 #include <stdio.h>
 #include "clasp.h"
+#include "core/numbers.h"
 #include "core/lispVector.h"
 #include "core/vectorObjects.h"
+#include "core/translators.h"  // assorted translators for string etc
 #include "core/cons.h"
 
+#define DIAG
 
 #define EXPORT __attribute__((visibility("default")))
 
@@ -24,6 +27,7 @@ public:
     void fill(const vector<double>& arg) {
         this->values.resize(arg.size());
         for (int i=0; i<arg.size(); ++i ) {
+	    printf("Filling %d --> %lf\n", i, arg[i]);
             this->values[i] = arg[i];
         }
     }
@@ -40,6 +44,13 @@ public:
             result[i] = (*this)[i]+y[i];
         }
     }
+    double at(int i) 
+    {
+	if ( i<0 || i >= this->values.size() ) {
+	    SIMPLE_ERROR(BF("Index out of bounds"));
+	}
+	return this->values[i]; 
+    };
     double dot(const DoubleVector& y) {
         if ( this->size() != y.size() ) {
             throw MismatchedSize();
@@ -50,6 +61,19 @@ public:
         }
         return dot;
     }
+
+    // Note: Here I'm setting up arguments as a Common Lisp lambda-list for the method.
+    // Just don't go nuts with initializers. :-)
+#define ARGS_dump "(self &optional (prefix \"entry\"))"
+#define DECL_dump ""
+#define DOCS_dump "Dump the vector using printf. Optionally provide a prefix string to print"
+    void dump(const string& prefix ) {
+	printf("Dumping double-vector\n");
+	for ( int i(0); i<this->values.size(); ++i ) {
+	    printf("%s[%3d] %lf\n", prefix.c_str(), i, this->values[i]);
+	}
+    }
+    
 };
 
 
@@ -60,12 +84,14 @@ namespace translate {
         {
             core::VectorObjects_sp vec;
             vec->adjust(_Nil<core::T_O>(),_Nil<core::Cons_O>(),v.size());
-            printf("%s:%d fill the vec here\n", __FILE__, __LINE__ );
+	    for ( int i(0); i<v.size(); ++i ) {
+		vec->setf_elt(i,core::DoubleFloat_O::create(v[i]));
+	    }
             return vec;
         }
     };
 
-    template <> struct from_object<vector<double> >
+    template <> struct from_object<const vector<double>& >
     {
         typedef vector<double> DeclareType;
         DeclareType _v;
@@ -83,7 +109,7 @@ namespace translate {
             } else if ( core::Vector_sp vec = obj.asOrNull<core::Vector_O>() ) {
                 // Translate a VECTOR of doubles into a vector<double>
                 this->_v.resize(vec->length());
-                for ( size_t idx; idx<vec->length(); ++idx ) {
+                for ( size_t idx(0); idx<vec->length(); ++idx ) {
                     this->_v[idx] = (*vec)[idx].as<core::Number_O>()->as_double();
                 }
             } else {
@@ -99,14 +125,17 @@ extern "C" {
     EXPORT
     void CLASP_MAIN()
     {
+	printf("Exporting functions\n");
         using namespace clbind;
         package("DV") [
             class_<DoubleVector>("double-vector" ,no_default_constructor )
             .   def_constructor("make-double-vector-with-size",constructor<int>())
-            .   def_constructor("make-double-vector-with-values",constructor<vector<double>>())
+            .   def_constructor("make-double-vector-with-values",constructor<const vector<double>&>())
             .   def("fill",&DoubleVector::fill)
             .   def("add",&DoubleVector::add)
             .   def("dot",&DoubleVector::dot)
+	    .   def("at",&DoubleVector::at)
+	    .   def("dump",&DoubleVector::dump,policies<>(),ARGS_dump,DECL_dump,DOCS_dump)
             ];
     }
 }
