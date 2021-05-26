d0e95783ec2ad7cc99db4e959da5b466d53886b0
CHRISTIAN SCHAFMEISTER <meister@fry.myfiosgateway.com>
2021-05-25 21:30:05 -0400

    Making hello-world more pedantic

--- hello-world/hello-world.cc
+++ hello-world/hello-world.cc
@@ -31,21 +31,51 @@ double addThreeNumbers_n_times(size_t n, double x, double y, double z)
 }
 
 
+enum ColorEnum { red, green, blue  };
+
+void printColor( ColorEnum color ) {
+  switch (color) {
+  case red:
+      printf("red\n");
+      break;
+  case green:
+      printf("green\n");
+      break;
+  case blue:
+      printf("blue\n");
+      break;
+  }
+}
+
 // ------------------------------------------------------------
 //
 // Set this code up so that it can be loaded into Clasp
 //
+
+PACKAGE_NICKNAME("HW");
+NAMESPACE_PACKAGE_ASSOCIATION(hw,HWPkg,"HELLO-WORLD");
+
+SYMBOL_EXPORT_SC_(HWPkg,STARcolorTranslatorSTAR);
+CLBIND_TRANSLATE_SYMBOL_TO_ENUM(ColorEnum, hw::_sym_STARcolorTranslatorSTAR );
 
 namespace hw {
 CL_EXPOSE
 void hello_world_startup() {
   printf("Entered %s:%d:%s\n", __FILE__, __LINE__, __FUNCTION__ );
   using namespace clbind;
-  package_ pkg("HW", {}, {} );
+  package_ pkg(HWPkg);
   scope_& s = pkg.scope();
   s.def("hello-world",&helloWorld);
   s.def("addThreeNumbers",&addThreeNumbers);
   s.def("addThreeSingleFloats",&addThreeSingleFloats);
   s.def("addThreeNumbers_n_times", &addThreeNumbers_n_times);
+  enum_<ColorEnum>(s,hw:_sym_STARcolorTranslatorSTAR)
+      .value("red",red)
+      .value("green",green)
+      .value("blue",blue);
+  s.def("printColor",&printColor);
 }
+
+
 };
+
